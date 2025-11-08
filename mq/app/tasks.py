4a650be798cccf3818ac2749022875a9a4b9618c
Jayant <jayant.danech@asians.cloud>
2025-11-08 18:13:23 +0530

    update

--- mq/app/tasks.py
+++ mq/app/tasks.py
@@ -2,7 +2,8 @@ from celery import Celery
 import time
 
 # Connect Celery to RabbitMQ broker
-app = Celery('tasks', broker='amqp://guest@rabbitmq//')
+#app = Celery('tasks', broker='amqp://guest@rabbitmq//')
+app = Celery('tasks', broker='amqp://guest:guest@rabbitmq:5672//')
 
 @app.task
 def process_data(x, y):
