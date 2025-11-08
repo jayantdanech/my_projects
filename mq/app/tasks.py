831cf2491cc54da876bbfe2a3613e42ebdaa5cde
Jayant <jayant.danech@asians.cloud>
2025-11-08 18:33:02 +0530

    update

--- mq/app/tasks.py
+++ mq/app/tasks.py
@@ -1,12 +1,14 @@
 from celery import Celery
 import time
 
-# Connect Celery to RabbitMQ broker
-#app = Celery('tasks', broker='amqp://guest@rabbitmq//')
-app = Celery('tasks', broker='amqp://guest:guest@rabbitmq:5672//')
+app = Celery(
+    'tasks',
+    broker='amqp://guest:guest@rabbitmq:5672//',
+    backend='redis://redis:6379/0'   # <-- Added this line
+)
 
 @app.task
 def process_data(x, y):
-    time.sleep(3)  # Simulate heavy work
+    time.sleep(3)
     return x + y
 
